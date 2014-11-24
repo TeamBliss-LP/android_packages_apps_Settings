607ba05a9e5ebe2f158486d1ba440f4de7124cf6
Evisceration <eviscerationls@gmail.com>
2014-11-24 12:58:50 +0100

    Settings: add navigation bar category
    Change-Id: I08a935cfbc5101d7ced1d4a828c05580937e6417
    
    Conflicts:
    	res/values/custom_strings.xml

--- src/com/android/settings/SettingsActivity.java
+++ src/com/android/settings/SettingsActivity.java
@@ -82,6 +82,7 @@ import com.android.settings.deviceinfo.Memory;
 import com.android.settings.deviceinfo.UsbSettings;
 import com.android.settings.fuelgauge.BatterySaverSettings;
 import com.android.settings.fuelgauge.PowerUsageSummary;
+import com.android.settings.nameless.NavigationBarSettings;
 import com.android.settings.notification.NotificationAppList;
 import com.android.settings.notification.OtherSoundSettings;
 import com.android.settings.quicklaunch.QuickLaunchSettings;
@@ -236,7 +237,8 @@ public class SettingsActivity extends Activity
             R.id.nfc_payment_settings,
             R.id.home_settings,
             R.id.dashboard,
-            R.id.privacy_settings_cyanogenmod
+            R.id.privacy_settings_cyanogenmod,
+            R.id.navigation_bar_settings
     };
 
     private static final String[] ENTRY_FRAGMENTS = {
@@ -302,7 +304,8 @@ public class SettingsActivity extends Activity
             QuickLaunchSettings.class.getName(),
             ApnSettings.class.getName(),
             BlacklistSettings.class.getName(),
-            com.android.settings.cyanogenmod.PrivacySettings.class.getName()
+            com.android.settings.cyanogenmod.PrivacySettings.class.getName(),
+            NavigationBarSettings.class.getName()
     };
 
 
