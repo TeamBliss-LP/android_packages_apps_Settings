fb6f70ab81a8dee6bab0d2f9e2c368d9ce0a3442
Jackeagle <jackeagle102@gmail.com>
2014-12-02 16:12:35 +0530

    Display Bliss Build Type In About Phone [1/2]
    Change-Id: Ibb93d55559c158646534cbd6edff6fa8df7822a5

--- src/com/android/settings/DeviceInfoSettings.java
+++ src/com/android/settings/DeviceInfoSettings.java
@@ -81,6 +81,7 @@ public class DeviceInfoSettings extends SettingsPreferenceFragment implements In
     private static final String KEY_SAFETY_LEGAL = "safetylegal";
     private static final String KEY_MOD_VERSION = "mod_version";
     private static final String KEY_MOD_BUILD_DATE = "build_date";
+    private static final String KEY_MOD_BUILD_TYPE = "build_type";
     private static final String KEY_BLISS_SHARE = "share";
 
     long[] mHits = new long[3];
@@ -104,6 +105,8 @@ public class DeviceInfoSettings extends SettingsPreferenceFragment implements In
         setValueSummary(KEY_MOD_VERSION, "ro.bliss.display.version");
         findPreference(KEY_MOD_VERSION).setEnabled(true);
         setValueSummary(KEY_MOD_BUILD_DATE, "ro.build.date");
+        setValueSummary(KEY_MOD_BUILD_TYPE, "ro.bliss.display.buildtype");
+        findPreference(KEY_MOD_BUILD_TYPE).setEnabled(true);
 
         if (!SELinux.isSELinuxEnabled()) {
             String status = getResources().getString(R.string.selinux_status_disabled);
