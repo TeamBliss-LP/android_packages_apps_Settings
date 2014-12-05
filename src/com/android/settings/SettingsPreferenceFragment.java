03cfb89974e2462f454480cd221b3f32d36696d6
John Brewer <gzroms@hotmail.com>
2014-12-05 08:57:53 -0500

    AOKP Animations
    Conflicts:
    	AndroidManifest.xml
    	res/values/validus_strings.xml
    	res/xml/animation_settings.xml
    
    Change-Id: I4f08e1bb8b2c27463fe24aefeafb7fafe1423db0

--- src/com/android/settings/SettingsPreferenceFragment.java
+++ src/com/android/settings/SettingsPreferenceFragment.java
@@ -58,6 +58,11 @@ public class SettingsPreferenceFragment extends PreferenceFragment implements Di
     private SettingsDialogFragment mDialogFragment;
 
     private String mHelpUrl;
+	
+    protected Context mContext;
+
+    // Need to use AOKP Custom system animation
+    protected ContentResolver mContentRes;	
 
     // Cache the content resolver for async callbacks
     private ContentResolver mContentResolver;
@@ -85,6 +90,11 @@ public class SettingsPreferenceFragment extends PreferenceFragment implements Di
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
+		
+	mContext = getActivity().getApplicationContext();
+
+	// Need to use AOKP Custom system animation
+	mContentRes = getActivity().getContentResolver();		
 
         if (icicle != null) {
             mPreferenceHighlighted = icicle.getBoolean(SAVE_HIGHLIGHTED_KEY);
@@ -513,4 +523,9 @@ public class SettingsPreferenceFragment extends PreferenceFragment implements Di
             return false;
         }
     }
+
+    // Need to AOKP Custom system animation
+    public void setTitle(int resId) {
+        getActivity().setTitle(resId);
+    }		
 }
