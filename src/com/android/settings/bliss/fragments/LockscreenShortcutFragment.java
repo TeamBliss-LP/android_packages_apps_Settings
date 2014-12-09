f9b4be8c28aa1ba11aebd2cf99e4d90f466d0b11
Lars Greiss <kufikugel@googlemail.com>
2014-12-09 15:27:48 +0100

    Settings:  allow device trees to invoke custom display settings
    in this case gesture control settings.
    
    Intent used: com.slim.action.LAUNCH_SCREEN_OFF_GESTURE_SETTINGS
    
    Due that the methods to inject external settings have changed on lp due of
    the dashboard we reintroduce a simply meta data preference method which can setup
    on wherever sub settings screen an entry with title and summary.
    
    Change-Id: I82f5a8a4075f109419b08014ad2d723bf2f54402
    
    Conflicts:
    	res/values/slim_strings.xml
    	src/com/android/settings/DisplaySettings.java

--- src/com/android/settings/bliss/fragments/LockscreenShortcutFragment.java
+++ src/com/android/settings/bliss/fragments/LockscreenShortcutFragment.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.android.settings.slim.fragments;
+package com.android.settings.bliss.fragments;
 
 import android.os.Bundle;
 import android.preference.Preference;
