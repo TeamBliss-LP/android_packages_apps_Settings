03700ed69bb01bbf549cdd43f622d9d683a11d24
Shaun Eaton <shaunsibu@gmail.com>
2014-12-10 11:38:22 -0700

    Revert Settings: allow device trees to invoke custom display settings https://github.com/TeamBliss-LP/android_packages_apps_Settings/commit/f9b4be8c28aa1ba11aebd2cf99e4d90f466d0b11
    Change-Id: Ib654f84a49d78de0fbd294a4a77365e10bc0ef66

--- src/com/android/settings/DisplaySettings.java
+++ src/com/android/settings/DisplaySettings.java
@@ -115,7 +115,6 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
     private static final String KEY_VOLUME_WAKE = "pref_volume_wake";
     private static final String KEY_DISPLAY_COLOR = "color_calibration";
     private static final String KEY_DISPLAY_GAMMA = "gamma_tuning";
-    private static final String KEY_SCREEN_OFF_GESTURE_SETTINGS = "screen_off_gesture_settings";
 
     private static final int DLG_GLOBAL_CHANGE_WARNING = 1;
 
@@ -334,10 +333,6 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
         if (counter == 2) {
             prefSet.removePreference(mWakeUpOptions);
         }
-
-        Utils.updatePreferenceToSpecificActivityFromMetaDataOrRemove(getActivity(),
-                getPreferenceScreen(), KEY_SCREEN_OFF_GESTURE_SETTINGS);
-
     }
 
     private static boolean allowAllRotations(Context context) {
