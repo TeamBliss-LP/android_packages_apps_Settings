89cf9d94e26c256ec0ab5e6b87774c0343110a3e
ezio84-xda <brabus84@gmail.com>
2014-12-29 16:14:08 +0100

    Battery style: fix dynamic percentage setting
    not being disabled after escaping from the statusbar
    settings page and going back to it
    (bug reported here: http://review.cyanogenmod.org/#/c/82420/)
    
    PS2: better int name thanks to Michael
    
    Change-Id: I3ddf69081866f6214c8880b6a4e2bab6fcd01b9c
    
    Conflicts:
    	src/com/android/settings/cyanogenmod/StatusBarSettings.java

--- src/com/android/settings/bliss/BatteryStyle.java
+++ src/com/android/settings/bliss/BatteryStyle.java
@@ -64,9 +64,8 @@ public class BatteryStyle extends SettingsPreferenceFragment
     private static final String PREF_BATT_ANIMATE = "battery_bar_animate";
     private static final String STATUS_BAR_SHOW_BATTERY_PERCENT = "status_bar_show_battery_percent";
     private static final String STATUS_BAR_BATTERY_STYLE = "status_bar_battery_style";
-    private static final String STATUS_BAR_BATTERY_STYLE_HIDDEN = "4";
-    private static final String STATUS_BAR_BATTERY_STYLE_TEXT = "6";
-    
+    private static final int STATUS_BAR_BATTERY_STYLE_HIDDEN = 4;
+    private static final int STATUS_BAR_BATTERY_STYLE_TEXT = 6;
 
     private ListPreference mStatusBarBattery;
     private ListPreference mStatusBarBatteryShowPercent;
@@ -157,6 +156,7 @@ public class BatteryStyle extends SettingsPreferenceFragment
                Settings.System.STATUS_BAR_SHOW_BATTERY_PERCENT, 0);
         mStatusBarBatteryShowPercent.setValue(String.valueOf(batteryShowPercent));
         mStatusBarBatteryShowPercent.setSummary(mStatusBarBatteryShowPercent.getEntry());
+        enableStatusBarBatteryDependents(batteryStyle);
         mStatusBarBatteryShowPercent.setOnPreferenceChangeListener(this);        
 
         setHasOptionsMenu(true);
@@ -207,8 +207,7 @@ public class BatteryStyle extends SettingsPreferenceFragment
             Settings.System.putInt(
                     resolver, Settings.System.STATUS_BAR_BATTERY_STYLE, batteryStyle);
             mStatusBarBattery.setSummary(mStatusBarBattery.getEntries()[index]);
-
-            enableStatusBarBatteryDependents((String) newValue);
+            enableStatusBarBatteryDependents(batteryStyle);
             return true;
         } else if (preference == mStatusBarBatteryShowPercent) {
             int batteryShowPercent = Integer.valueOf((String) newValue);
@@ -248,9 +247,12 @@ public class BatteryStyle extends SettingsPreferenceFragment
             mBatteryBarColor.setEnabled(true);
         }
     }
-    private void enableStatusBarBatteryDependents(String value) {
-        boolean enabled = !(value.equals(STATUS_BAR_BATTERY_STYLE_TEXT)
-                || value.equals(STATUS_BAR_BATTERY_STYLE_HIDDEN));
-        mStatusBarBatteryShowPercent.setEnabled(enabled);
+    private void enableStatusBarBatteryDependents(int batteryIconStyle) {
+        if (batteryIconStyle == STATUS_BAR_BATTERY_STYLE_HIDDEN ||
+                batteryIconStyle == STATUS_BAR_BATTERY_STYLE_TEXT) {
+            mStatusBarBatteryShowPercent.setEnabled(false);
+        } else {
+            mStatusBarBatteryShowPercent.setEnabled(true);
+        }
     }    
 }
