2c1c831a949a090e2a483054494531ce1a5cde9e
Patrick L <devvortex@gmail.com>
2014-12-31 08:34:57 -0500

    Fix screen timeout preference summary
    Would show the index one less than the actual selected one.

--- src/com/android/settings/DisplaySettings.java
+++ src/com/android/settings/DisplaySettings.java
@@ -395,7 +395,7 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
                 for (int i = 0; i < values.length; i++) {
                     long timeout = Long.parseLong(values[i].toString());
                     try {
-                        if (currentTimeout > timeout) {
+                        if (currentTimeout >= timeout) {
                            best = i;
                         }
                     } catch (Exception e) {
