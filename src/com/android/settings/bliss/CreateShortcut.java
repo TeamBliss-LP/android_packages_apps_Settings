VanirAOSP/packages_apps_Settings

Squash of Dave's commits for Navi (2/2)
Inception to 1/4/15.  For testing

Vanir actions and utilities [2/2]

activities for vanir actions and utilities

Change-Id: Ie8c8faed8b1309442b71899b32f5acb31ccc228d
Signed-off-by: DHOMD <DHO@dho.im>
Conflicts:
	AndroidManifest.xml

Customizable navbar [2/2]

Change-Id: I3481687dbd7bc7de903ab9266a0473b33185f5ff
Signed-off-by: DHOMD <DHO@dho.im>

Conflicts:
	res/values/vanir_arrays.xml
	res/values/vanir_dimens.xml
	res/values/vanir_strings.xml

Cleanup

Tabs,whitespaces

Change-Id: I90488a92c80b764a91acc6eada8d91a2c223c9b8
Signed-off-by: DHOMD <DHO@dho.im>
Conflicts:
	res/values/vanir_arrays.xml
	res/values/vanir_dimens.xml
	res/values/vanir_strings.xml
	res/xml/vanir_interface.xml

ArrangeNavbarFragment: fix race condition

Change-Id: Ic854476db3478d31637ac1b1fb6860ef12c88b80

Navbar icons: since DocumentsUI is a megafail lets guide users to gallery

Change-Id: Ic9b179a24634ec7ca1d407a7f9ae2a581dd9d307
Signed-off-by: DHOMD <DHO@dho.im>

Conflicts:
	res/values/vanir_strings.xml

Navring toggle: make sure to use the correct default value

Change-Id: I641731882792bbf3eab7cc9c9e48556b470aaa39

src/com/android/settings/vanir/CreateShortcut.java
@@ -1,0 +1,90 @@
+/*
+* Copyright (C) 2014 VanirAOSP && the Android Open Source Project
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+package com.android.settings.vanir;
+
+import android.app.LauncherActivity;
+import android.content.Intent;
+import android.util.Log;
+import android.view.View;
+import android.widget.ListView;
+
+import com.android.settings.R;
+
+// Vanir actions and utilities setup
+
+public class CreateShortcut extends LauncherActivity {
+
+    @Override
+    protected Intent getTargetIntent() {
+        Intent targetIntent = new Intent(Intent.ACTION_MAIN, null);
+        targetIntent.addCategory("com.android.systemui.SHORTCUT");
+        targetIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        return targetIntent;
+    }
+
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        Intent shortcutIntent = intentForPosition(position);
+
+        String intentClass = shortcutIntent.getComponent().getClassName();
+        String intentAction = shortcutIntent.getAction();
+
+        shortcutIntent = new Intent();
+        shortcutIntent.setClassName(this, intentClass);
+        shortcutIntent.setAction(intentAction);
+
+        Intent intent = new Intent();
+        intent.putExtra(Intent.EXTRA_SHORTCUT_ICON_RESOURCE,
+        Intent.ShortcutIconResource.fromContext(this, getProperShortcutIcon(intentClass)));
+        intent.putExtra(Intent.EXTRA_SHORTCUT_INTENT, shortcutIntent);
+        intent.putExtra(Intent.EXTRA_SHORTCUT_NAME, itemForPosition(position).label);
+        setResult(RESULT_OK, intent);
+        finish();
+    }
+
+    private int getProperShortcutIcon(String className) {
+        String c = className.substring(className.lastIndexOf(".") + 1);
+
+        if (c.equals ("FlushMemory")) {
+            return R.drawable.vanir_flush_memory;
+
+        } else if (c.equals ("RingVibToggle")) {
+            return R.drawable.ic_lockscreen_vib;
+
+        } else if (c.equals ("RingSilentToggle")) {
+            return R.drawable.ic_lockscreen_silent;
+
+        } else if (c.equals ("RingVibSilentToggle")) {
+            return R.drawable.ic_lockscreen_soundon;
+
+        } else if (c.equals ("StartDreamActivity")) {
+            return R.drawable.ic_sysbar_dream;
+
+        } else if (c.equals ("ProfileActivity")) {
+            return R.drawable.ic_sysbar_profile;
+
+        } else {
+            return R.drawable.ic_sysbar_null;
+        }
+    }
+
+    @Override
+    protected boolean onEvaluateShowIcons() {
+        return false;
+    }
+}
+

src/com/android/settings/vanir/activities/FlushMemory.java
@@ -1,0 +1,67 @@
+/*
+ * Copyright 2013 VanirAOSP && the Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.vanir.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.vanir.util.CMDProcessor;
+import com.vanir.util.CMDProcessor.CommandResult;
+import com.vanir.util.Helpers;
+
+public class FlushMemory extends Activity {
+
+  private static final String TAG = "VanirMemFlusher";
+
+  public FlushMemory() {
+    super();
+  }
+
+  /** Called when the activity is first created. */
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+  }
+
+  @Override
+  public void onResume() {
+    super.onResume();
+
+    Flush();
+
+    this.finish();
+  }
+
+  public static void Flush() {
+    // flush page cache, inodes, and dentries, finalize, and suggest a
+    // garbage collection to the VM.  Useful to run before/after something
+    // memory intensive like a game.  This will keep all your apps in memory
+    // Log.v(TAG, "(1/3) Diddling drop_caches");
+
+    //drop_caches 1 drops stuff that is dropped as part of drop_caches 3, so 3 makes 1 useless
+    // CommandResult cr = new CMDProcessor().su.runWaitFor("echo 3 > /proc/sys/vm/drop_caches; sync");
+    // if (!cr.success())
+    //    Log.w(TAG, "Failed: echo 3 > /proc/sys/vm/drop_caches; sync\n\t"+cr.toString());
+
+    Log.v(TAG, "(1/2) Running finalization");
+    System.runFinalization();
+    Log.v(TAG, "(2/2) Suggesting garbage collection to the VM");
+    System.gc();
+    Log.i(TAG, "Flush complete");
+  }
+}

src/com/android/settings/vanir/activities/RingVibSilentToggle.java
@@ -1,0 +1,67 @@
+/*
+ * Copyright 2011 AOKP by Mike Wilson - Zaphod-Beeblebrox
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.vanir.activities;
+
+import android.app.Activity;
+import android.content.Context;
+import android.media.AudioManager;
+import android.media.ToneGenerator;
+import android.os.Bundle;
+import android.os.Vibrator;
+import android.provider.Settings;
+
+/*
+ * Toggle Ring/Vibrate/Silent
+ */
+
+public class RingVibSilentToggle extends Activity  {
+
+    public RingVibSilentToggle() {
+        super();
+    }
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        AudioManager am = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+        if (am != null) {
+            if (am.getRingerMode() == AudioManager.RINGER_MODE_NORMAL) {
+                am.setRingerMode(AudioManager.RINGER_MODE_VIBRATE);
+                Vibrator vib = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+                if (vib != null) {
+                    vib.vibrate(50);
+                }
+            } else if (am.getRingerMode() == AudioManager.RINGER_MODE_VIBRATE) {
+                am.setRingerMode(AudioManager.RINGER_MODE_SILENT);
+            } else {
+                am.setRingerMode(AudioManager.RINGER_MODE_NORMAL);
+                ToneGenerator tg = new ToneGenerator(AudioManager.STREAM_NOTIFICATION, (int)(ToneGenerator.MAX_VOLUME * 0.85));
+                if (tg != null) {
+                    tg.startTone(ToneGenerator.TONE_PROP_BEEP);
+                }
+            }
+        }
+    finish();
+    }
+}

src/com/android/settings/vanir/util/DragSortItemView.java
@@ -1,0 +1,118 @@
+/**
+ * A subclass of the Android ListView component that enables drag
+ * and drop re-ordering of list items.
+ *
+ * Copyright 2012 Carl Bauer
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.vanir.util;
+
+import android.content.Context;
+import android.view.Gravity;
+import android.view.View;
+import android.view.View.MeasureSpec;
+import android.view.ViewGroup;
+import android.widget.AbsListView;
+import android.util.Log;
+
+/**
+ * Lightweight ViewGroup that wraps list items obtained from user's
+ * ListAdapter. ItemView expects a single child that has a definite
+ * height (i.e. the child's layout height is not MATCH_PARENT).
+ * The width of
+ * ItemView will always match the width of its child (that is,
+ * the width MeasureSpec given to ItemView is passed directly
+ * to the child, and the ItemView measured width is set to the
+ * child's measured width). The height of ItemView can be anything;
+ *
+ *
+ * The purpose of this class is to optimize slide
+ * shuffle animations.
+ */
+public class DragSortItemView extends ViewGroup {
+
+    private int mGravity = Gravity.TOP;
+
+    public DragSortItemView(Context context) {
+        super(context);
+
+        // always init with standard ListView layout params
+        setLayoutParams(new AbsListView.LayoutParams(
+                LayoutParams.FILL_PARENT,
+                LayoutParams.WRAP_CONTENT));
+
+        setClipChildren(true);
+    }
+
+    public void setGravity(int gravity) {
+        mGravity = gravity;
+    }
+
+    public int getGravity() {
+        return mGravity;
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
+        final View child = getChildAt(0);
+
+        if (child == null) {
+            return;
+        }
+
+        if (mGravity == Gravity.TOP) {
+            child.layout(0, 0, getMeasuredWidth(), child.getMeasuredHeight());
+        } else {
+            child.layout(0, getMeasuredHeight() - child.getMeasuredHeight(), getMeasuredWidth(), getMeasuredHeight());
+        }
+    }
+
+    /**
+     * 
+     */
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+
+        int height = MeasureSpec.getSize(heightMeasureSpec);
+        int width = MeasureSpec.getSize(widthMeasureSpec);
+
+        int heightMode = MeasureSpec.getMode(heightMeasureSpec);
+
+        final View child = getChildAt(0);
+        if (child == null) {
+            setMeasuredDimension(0, width);
+            return;
+        }
+
+        if (child.isLayoutRequested()) {
+            // Always let child be as tall as it wants.
+            measureChild(child, widthMeasureSpec,
+                    MeasureSpec.makeMeasureSpec(0, MeasureSpec.UNSPECIFIED));
+        }
+
+        if (heightMode == MeasureSpec.UNSPECIFIED) {
+            LayoutParams lp = getLayoutParams();
+
+            if (lp.height > 0) {
+                height = lp.height;
+            } else {
+                height = child.getMeasuredHeight();
+            }
+        }
+
+        setMeasuredDimension(width, height);
+    }
+
+}

TeamBliss-LP/android_packages_apps_Settings

Signed-off-by: scott <scott@ubuntu>
	res/xml/dashboard_categories.xml
	res/layout/ga_create_gesture.xml
	res/layout/ga_gestures_list.xml
	src/com/android/settings/beanstalk/gestureanywhere/GestureAnywhereCreateGestureActivity.java
	res/values/cm_dimens.xml
	res/xml/beanstalk_settings.xml
	src/com/android/settings/beanstalk/MainSettings.java

src/com/android/settings/bliss/CreateShortcut.java
@@ -1,0 +1,90 @@
+/*
+* Copyright (C) 2014 VanirAOSP && the Android Open Source Project
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+package com.android.settings.bliss;
+
+import android.app.LauncherActivity;
+import android.content.Intent;
+import android.util.Log;
+import android.view.View;
+import android.widget.ListView;
+
+import com.android.settings.R;
+
+// Vanir actions and utilities setup
+
+public class CreateShortcut extends LauncherActivity {
+
+    @Override
+    protected Intent getTargetIntent() {
+        Intent targetIntent = new Intent(Intent.ACTION_MAIN, null);
+        targetIntent.addCategory("com.android.systemui.SHORTCUT");
+        targetIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        return targetIntent;
+    }
+
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        Intent shortcutIntent = intentForPosition(position);
+
+        String intentClass = shortcutIntent.getComponent().getClassName();
+        String intentAction = shortcutIntent.getAction();
+
+        shortcutIntent = new Intent();
+        shortcutIntent.setClassName(this, intentClass);
+        shortcutIntent.setAction(intentAction);
+
+        Intent intent = new Intent();
+        intent.putExtra(Intent.EXTRA_SHORTCUT_ICON_RESOURCE,
+        Intent.ShortcutIconResource.fromContext(this, getProperShortcutIcon(intentClass)));
+        intent.putExtra(Intent.EXTRA_SHORTCUT_INTENT, shortcutIntent);
+        intent.putExtra(Intent.EXTRA_SHORTCUT_NAME, itemForPosition(position).label);
+        setResult(RESULT_OK, intent);
+        finish();
+    }
+
+    private int getProperShortcutIcon(String className) {
+        String c = className.substring(className.lastIndexOf(".") + 1);
+
+        if (c.equals ("FlushMemory")) {
+            return R.drawable.vanir_flush_memory;
+
+        } else if (c.equals ("RingVibToggle")) {
+            return R.drawable.ic_lockscreen_vib;
+
+        } else if (c.equals ("RingSilentToggle")) {
+            return R.drawable.ic_lockscreen_silent;
+
+        } else if (c.equals ("RingVibSilentToggle")) {
+            return R.drawable.ic_lockscreen_soundon;
+
+        } else if (c.equals ("StartDreamActivity")) {
+            return R.drawable.ic_sysbar_dream;
+
+        } else if (c.equals ("ProfileActivity")) {
+            return R.drawable.ic_sysbar_profile;
+
+        } else {
+            return R.drawable.ic_sysbar_null;
+        }
+    }
+
+    @Override
+    protected boolean onEvaluateShowIcons() {
+        return false;
+    }
+}
+

src/com/android/settings/bliss/activities/FlushMemory.java
@@ -1,0 +1,67 @@
+/*
+ * Copyright 2013 VanirAOSP && the Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.bliss.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.bliss.util.CMDProcessor;
+import com.bliss.util.CMDProcessor.CommandResult;
+import com.bliss.util.Helpers;
+
+public class FlushMemory extends Activity {
+
+  private static final String TAG = "VanirMemFlusher";
+
+  public FlushMemory() {
+    super();
+  }
+
+  /** Called when the activity is first created. */
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+  }
+
+  @Override
+  public void onResume() {
+    super.onResume();
+
+    Flush();
+
+    this.finish();
+  }
+
+  public static void Flush() {
+    // flush page cache, inodes, and dentries, finalize, and suggest a
+    // garbage collection to the VM.  Useful to run before/after something
+    // memory intensive like a game.  This will keep all your apps in memory
+    // Log.v(TAG, "(1/3) Diddling drop_caches");
+
+    //drop_caches 1 drops stuff that is dropped as part of drop_caches 3, so 3 makes 1 useless
+    // CommandResult cr = new CMDProcessor().su.runWaitFor("echo 3 > /proc/sys/vm/drop_caches; sync");
+    // if (!cr.success())
+    //    Log.w(TAG, "Failed: echo 3 > /proc/sys/vm/drop_caches; sync\n\t"+cr.toString());
+
+    Log.v(TAG, "(1/2) Running finalization");
+    System.runFinalization();
+    Log.v(TAG, "(2/2) Suggesting garbage collection to the VM");
+    System.gc();
+    Log.i(TAG, "Flush complete");
+  }
+}

src/com/android/settings/bliss/activities/RingVibSilentToggle.java
@@ -1,0 +1,67 @@
+/*
+ * Copyright 2011 AOKP by Mike Wilson - Zaphod-Beeblebrox
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.bliss.activities;
+
+import android.app.Activity;
+import android.content.Context;
+import android.media.AudioManager;
+import android.media.ToneGenerator;
+import android.os.Bundle;
+import android.os.Vibrator;
+import android.provider.Settings;
+
+/*
+ * Toggle Ring/Vibrate/Silent
+ */
+
+public class RingVibSilentToggle extends Activity  {
+
+    public RingVibSilentToggle() {
+        super();
+    }
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        AudioManager am = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+        if (am != null) {
+            if (am.getRingerMode() == AudioManager.RINGER_MODE_NORMAL) {
+                am.setRingerMode(AudioManager.RINGER_MODE_VIBRATE);
+                Vibrator vib = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+                if (vib != null) {
+                    vib.vibrate(50);
+                }
+            } else if (am.getRingerMode() == AudioManager.RINGER_MODE_VIBRATE) {
+                am.setRingerMode(AudioManager.RINGER_MODE_SILENT);
+            } else {
+                am.setRingerMode(AudioManager.RINGER_MODE_NORMAL);
+                ToneGenerator tg = new ToneGenerator(AudioManager.STREAM_NOTIFICATION, (int)(ToneGenerator.MAX_VOLUME * 0.85));
+                if (tg != null) {
+                    tg.startTone(ToneGenerator.TONE_PROP_BEEP);
+                }
+            }
+        }
+    finish();
+    }
+}

src/com/android/settings/bliss/util/DragSortItemView.java
@@ -1,0 +1,118 @@
+/**
+ * A subclass of the Android ListView component that enables drag
+ * and drop re-ordering of list items.
+ *
+ * Copyright 2012 Carl Bauer
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.bliss.util;
+
+import android.content.Context;
+import android.view.Gravity;
+import android.view.View;
+import android.view.View.MeasureSpec;
+import android.view.ViewGroup;
+import android.widget.AbsListView;
+import android.util.Log;
+
+/**
+ * Lightweight ViewGroup that wraps list items obtained from user's
+ * ListAdapter. ItemView expects a single child that has a definite
+ * height (i.e. the child's layout height is not MATCH_PARENT).
+ * The width of
+ * ItemView will always match the width of its child (that is,
+ * the width MeasureSpec given to ItemView is passed directly
+ * to the child, and the ItemView measured width is set to the
+ * child's measured width). The height of ItemView can be anything;
+ *
+ *
+ * The purpose of this class is to optimize slide
+ * shuffle animations.
+ */
+public class DragSortItemView extends ViewGroup {
+
+    private int mGravity = Gravity.TOP;
+
+    public DragSortItemView(Context context) {
+        super(context);
+
+        // always init with standard ListView layout params
+        setLayoutParams(new AbsListView.LayoutParams(
+                LayoutParams.FILL_PARENT,
+                LayoutParams.WRAP_CONTENT));
+
+        setClipChildren(true);
+    }
+
+    public void setGravity(int gravity) {
+        mGravity = gravity;
+    }
+
+    public int getGravity() {
+        return mGravity;
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
+        final View child = getChildAt(0);
+
+        if (child == null) {
+            return;
+        }
+
+        if (mGravity == Gravity.TOP) {
+            child.layout(0, 0, getMeasuredWidth(), child.getMeasuredHeight());
+        } else {
+            child.layout(0, getMeasuredHeight() - child.getMeasuredHeight(), getMeasuredWidth(), getMeasuredHeight());
+        }
+    }
+
+    /**
+     * 
+     */
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+
+        int height = MeasureSpec.getSize(heightMeasureSpec);
+        int width = MeasureSpec.getSize(widthMeasureSpec);
+
+        int heightMode = MeasureSpec.getMode(heightMeasureSpec);
+
+        final View child = getChildAt(0);
+        if (child == null) {
+            setMeasuredDimension(0, width);
+            return;
+        }
+
+        if (child.isLayoutRequested()) {
+            // Always let child be as tall as it wants.
+            measureChild(child, widthMeasureSpec,
+                    MeasureSpec.makeMeasureSpec(0, MeasureSpec.UNSPECIFIED));
+        }
+
+        if (heightMode == MeasureSpec.UNSPECIFIED) {
+            LayoutParams lp = getLayoutParams();
+
+            if (lp.height > 0) {
+                height = lp.height;
+            } else {
+                height = child.getMeasuredHeight();
+            }
+        }
+
+        setMeasuredDimension(width, height);
+    }
+
+}