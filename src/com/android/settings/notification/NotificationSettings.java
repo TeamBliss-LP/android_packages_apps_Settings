a8e56fe3a5c46ce9ffca5ccb2c583bb43a753d5b
maxwen <max.weninger@gmail.com>
2015-01-12 20:08:14 +0100

    Settings: add setting for default alarm tone
    This one will be used when a new alarm is created
    
    Change-Id: Ia823122db804c800b236dc8741abb81c4cad7754

--- src/com/android/settings/notification/NotificationSettings.java
+++ src/com/android/settings/notification/NotificationSettings.java
@@ -67,6 +67,7 @@ public class NotificationSettings extends SettingsPreferenceFragment implements 
     private static final String KEY_NOTIFICATION_VOLUME = "notification_volume";
     private static final String KEY_PHONE_RINGTONE = "ringtone";
     private static final String KEY_NOTIFICATION_RINGTONE = "notification_ringtone";
+    private static final String KEY_ALARM_RINGTONE = "alarm_ringtone";
     private static final String KEY_VIBRATE_WHEN_RINGING = "vibrate_when_ringing";
     private static final String KEY_NOTIFICATION = "notification";
     private static final String KEY_NOTIFICATION_PULSE = "notification_pulse";
@@ -112,6 +113,7 @@ public class NotificationSettings extends SettingsPreferenceFragment implements 
 
     private ListPreference mHeadsUpSnoozeTime;
     private ListPreference mHeadsUpTimeOut;
+    private Preference mAlarmRingtonePreference;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -312,6 +314,13 @@ public class NotificationSettings extends SettingsPreferenceFragment implements 
                         mContext, RingtoneManager.TYPE_NOTIFICATION);
                 if (summary != null) {
                     mHandler.obtainMessage(H.UPDATE_NOTIFICATION_RINGTONE, summary).sendToTarget();
+                }
+            }
+            if (mAlarmRingtonePreference != null) {
+                final CharSequence summary = updateRingtoneName(
+                        mContext, RingtoneManager.TYPE_ALARM);
+                if (summary != null) {
+                    mHandler.obtainMessage(H.UPDATE_ALARM_RINGTONE, summary).sendToTarget();
                 }
             }
         }
@@ -583,6 +592,7 @@ public class NotificationSettings extends SettingsPreferenceFragment implements 
         private static final int UPDATE_NOTIFICATION_RINGTONE = 2;
         private static final int STOP_SAMPLE = 3;
         private static final int UPDATE_RINGER_ICON = 4;
+        private static final int UPDATE_ALARM_RINGTONE = 5;
 
         private H() {
             super(Looper.getMainLooper());
@@ -603,6 +613,10 @@ public class NotificationSettings extends SettingsPreferenceFragment implements 
                 case UPDATE_RINGER_ICON:
                     updateRingIcon(msg.arg1);
                     break;
+                case UPDATE_ALARM_RINGTONE:
+                    mAlarmRingtonePreference.setSummary((CharSequence) msg.obj);
+                    break;
+
             }
         }
     }
