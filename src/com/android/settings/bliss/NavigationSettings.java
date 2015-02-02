5140135d3b667dd5d61090efd4a7aa421d153676
Shaun Eaton <shaunsibu@gmail.com>
2015-02-01 23:41:03 -0700

    Create Navigation Options Preference Screen and move the following. - Navigation Bar - Buttons Settings - Pie Settings
    Change-Id: I46dc7a6a5e2e47c9d37f4c5d0b02c8e8407625a2

---
+++ src/com/android/settings/bliss/NavigationSettings.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright (C) 2013 SlimRoms
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.bliss;
+
+import android.app.ActivityManager;
+import android.app.AlertDialog;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.os.RemoteException;
+import android.os.SystemProperties;
+import android.provider.Settings;
+import android.preference.Preference;
+import android.preference.PreferenceScreen;
+import android.preference.PreferenceCategory;
+import android.preference.Preference.OnPreferenceChangeListener;
+import android.preference.SwitchPreference;
+import android.text.Spannable;
+import android.util.Log;
+import android.view.WindowManagerGlobal;
+import android.widget.EditText;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.Utils;
+
+import java.util.List;
+
+public class NavigationSettings extends SettingsPreferenceFragment {
+    private static final String TAG = "SystemSettings";
+    private static final String CATEGORY_NAVBAR = "navigation_bar";
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.bliss_navigation_settings);
+        PreferenceScreen prefSet = getPreferenceScreen();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+    
+
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+    return false;
+   }    
+}
