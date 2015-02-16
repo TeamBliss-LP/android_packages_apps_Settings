fd79260ce92f83340246b0d9307c7ab1e710cbe3
Yanuar Harry <ai.the.smarties.physics@gmail.com>
2015-02-16 20:21:52 +0100

    Settings: Ambient Display configurations
    AICPfy
    Move all to system settings
    Add SystemSettingsCheckBoxPreference that we had in kk
    PS2: Added Time category and played with strings
    
    Change-Id: Ib85c25d50dc924d4f07658dac033e6581184317d

--- src/com/android/settings/search/SearchIndexableResources.java
+++ src/com/android/settings/search/SearchIndexableResources.java
@@ -32,6 +32,7 @@ import com.android.settings.R;
 import com.android.settings.SecuritySettings;
 import com.android.settings.WirelessSettings;
 import com.android.settings.accessibility.AccessibilitySettings;
+import com.android.settings.bliss.AmbientSettings;
 import com.android.settings.bluetooth.BluetoothSettings;
 import com.android.settings.bliss.LockScreenSettings;
 import com.android.settings.bliss.NotificationDrawer;
@@ -302,6 +303,13 @@ public final class SearchIndexableResources {
                         R.xml.privacy_settings_cyanogenmod,
                         com.android.settings.cyanogenmod.PrivacySettings.class.getName(),
                         R.drawable.ic_settings_privacy));
+
+        sResMap.put(AmbientSettings.class.getName(),
+                new SearchIndexableResource(
+                        Ranking.getRankForClassName(AmbientSettings.class.getName()),
+                        NO_DATA_RES_ID,
+                        AmbientSettings.class.getName(),
+                        R.drawable.ic_settings_display));
     }
 
     private SearchIndexableResources() {
