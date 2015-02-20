1c5cc7624892131fef9caa1f9dd6f282d97eb387
Alex Cruz <mazdarider23@gmail.com>
2015-02-20 06:09:34 +0000

    NavBar tile [2/3]
    Change-Id: I684e370b813d03782d01e2abc0d28f06560a4cf0

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -111,5 +111,6 @@ public class Settings extends SettingsActivity {
     public static class ExpandedDesktopSettingsActivity extends SettingsActivity { /* empty */ }
     public static class SoundSettingsActivity extends SettingsActivity { /* empty */ }
     public static class LockScreenSettingsActivity extends SettingsActivity { /* empty */ }
+    public static class NavBarActivity extends SettingsActivity { /* empty */ }
 }
 
