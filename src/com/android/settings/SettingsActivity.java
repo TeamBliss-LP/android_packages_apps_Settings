30c5ee2100a705d59088993a5b12c3ea8eae82c5
Michael Bestas <mikeioannina@gmail.com>
2015-02-28 21:30:04 +0200

    Settings: Cleanup battery saver settings
    * Add back auto battery saver
    * Add back support for battery saver on devices without perf profiles
    
    Change-Id: I562420a81592fde54a96dca88a71ed9137bb59a9

--- src/com/android/settings/SettingsActivity.java
+++ src/com/android/settings/SettingsActivity.java
@@ -91,7 +91,6 @@ import com.android.settings.dashboard.NoHomeDialogFragment;
 import com.android.settings.dashboard.SearchResultsSummary;
 import com.android.settings.deviceinfo.Memory;
 import com.android.settings.deviceinfo.UsbSettings;
-import com.android.settings.fuelgauge.BatterySaverSettings;
 import com.android.settings.fuelgauge.PowerUsageSummary;
 import com.android.settings.cyanogenmod.DisplayRotation;
 import com.android.settings.cyanogenmod.qs.QSTiles;
@@ -315,7 +314,6 @@ public class SettingsActivity extends Activity
             ChooseLockPassword.ChooseLockPasswordFragment.class.getName(),
             ChooseLockPattern.ChooseLockPatternFragment.class.getName(),
             InstalledAppDetails.class.getName(),
-            BatterySaverSettings.class.getName(),
             NotificationAppList.class.getName(),
             AppNotificationSettings.class.getName(),
             OtherSoundSettings.class.getName(),
