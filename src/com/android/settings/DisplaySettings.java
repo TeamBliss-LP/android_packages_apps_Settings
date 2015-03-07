06be2fe93b14f6e9a9ec866d83662a321ae97e5b
Konsta <konsta09@gmail.com>
2015-03-07 21:09:56 +0200

    Settings: Fix hiding proximity wake-up option
    It's in 'Screen' category, not in 'Interface'.
    
    Change-Id: I270ed469188526177876026fbc1fe9a5762bea0e

--- src/com/android/settings/DisplaySettings.java
+++ src/com/android/settings/DisplaySettings.java
@@ -242,8 +242,8 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
         mDisplayDensityOverride.setOnPreferenceChangeListener(this);        
         boolean proximityCheckOnWake = getResources().getBoolean(
                 com.android.internal.R.bool.config_proximityCheckOnWake);
-        if (interfacePrefs != null && !proximityCheckOnWake) {
-            interfacePrefs.removePreference(findPreference(KEY_PROXIMITY_WAKE));
+        if (displayPrefs != null && !proximityCheckOnWake) {
+            displayPrefs.removePreference(findPreference(KEY_PROXIMITY_WAKE));
             Settings.System.putInt(getContentResolver(), Settings.System.PROXIMITY_ON_WAKE, 1);
         }
 
