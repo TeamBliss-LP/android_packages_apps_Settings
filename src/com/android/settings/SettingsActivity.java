AOKP/packages_apps_Settings

QS: Squashed from EuphoriaOS (2/2)
AICPfy

Change-Id: If9378713650e7ad74d647c36f788771559ed596c

src/com/android/settings/SettingsActivity.java
@@ -77,6 +77,8 @@ import com.android.settings.applications.ManageApplications;
 import com.android.settings.applications.ProcessStatsUi;
 import com.android.settings.blacklist.BlacklistSettings;
 import com.android.settings.bluetooth.BluetoothSettings;
+import com.android.settings.slim.DisplayRotation;
+import com.android.settings.cyanogenmod.qs.QSTiles;
 import com.android.settings.dashboard.DashboardCategory;
 import com.android.settings.dashboard.DashboardSummary;
 import com.android.settings.dashboard.DashboardTile;
@@ -315,7 +317,9 @@ public class SettingsActivity extends Activity
             ApnSettings.class.getName(),
             AicpSettings.class.getName(),
             BlacklistSettings.class.getName(),
-            com.android.settings.cyanogenmod.PrivacySettings.class.getName()
+            com.android.settings.cyanogenmod.PrivacySettings.class.getName(),
+            QSTiles.class.getName(),
+            DisplayRotation.class.getName()
     };
 
 

TeamBliss-LP/android_packages_apps_Settings

src/com/android/settings/SettingsActivity.java
@@ -88,6 +88,8 @@ import com.android.settings.deviceinfo.UsbSettings;
 import com.android.settings.fuelgauge.PowerUsageSummary;
 import com.android.settings.livedisplay.LiveDisplay;
 import com.android.settings.lockscreen.LockScreenSettings;
+import com.android.settings.cyanogenmod.DisplayRotation;
+import com.android.settings.cyanogenmod.qs.QSTiles;
 import com.android.settings.notification.NotificationAppList;
 import com.android.settings.notification.NotificationManagerSettings;
 import com.android.settings.notification.OtherSoundSettings;
@@ -322,7 +324,9 @@ public class SettingsActivity extends Activity
             com.android.settings.cyanogenmod.PrivacySettings.class.getName(),
             NotificationManagerSettings.class.getName(),
             LockScreenSettings.class.getName(),
-            LiveDisplay.class.getName()
+            LiveDisplay.class.getName(),
+            QSTiles.class.getName(),
+            DisplayRotation.class.getName()
     };
 
 