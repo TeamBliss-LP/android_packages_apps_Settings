62ee16c8edaef3a30cff09c70bf6dc854f3a3299
Nir Bruderman <nbruderman@gmail.com>
2015-04-25 19:48:20 +0000

    Revert "Settings: use correct default value for backlight brightness"
    This causes to lose the ability to turn off the backlight brightness at all!
    On every boot the values resets back to default now.
    
    This reverts commit a3a044f17d6a11508507e5ed083dac157dd3bd6d.
    
    Change-Id: Ib37bef026d56a32a0ff36655be4b9890a2d9636d

--- src/com/android/settings/ButtonSettings.java
+++ src/com/android/settings/ButtonSettings.java
@@ -690,9 +690,11 @@ public class ButtonSettings extends SettingsPreferenceFragment implements
             Settings.Secure.putInt(context.getContentResolver(),
                     Settings.Secure.BUTTON_BRIGHTNESS, 0);
         } else {
-            int oldBright = prefs.getInt("pre_navbar_button_backlight", defaultBrightness);
-            Settings.Secure.putInt(context.getContentResolver(),
-                    Settings.Secure.BUTTON_BRIGHTNESS, oldBright);
+            int oldBright = prefs.getInt("pre_navbar_button_backlight", -1);
+            if (oldBright != -1) {
+                Settings.Secure.putInt(context.getContentResolver(),
+                        Settings.Secure.BUTTON_BRIGHTNESS, oldBright);
+            }
         }
     }
 
