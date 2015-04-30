5425e0926456647dbf40bbd04249be159d6f2a35
rogersb11 <brettrogers11@gmail.com>
2015-04-30 03:33:10 -0400

    Add back TRDS Tile 2/2
    Change-Id: I5c85f060f4bb219b74b18dadc91636813b0aa634

--- src/com/android/settings/cyanogenmod/qs/QSTileHolder.java
+++ src/com/android/settings/cyanogenmod/qs/QSTileHolder.java
@@ -219,7 +219,11 @@ public class QSTileHolder {
                 break;
             case QSConstants.TILE_MUSIC:
                 resourceName = "ic_qs_media_play";
-                stringId = R.string.qs_music_play_tile;
+                stringId = R.string.qs_tile_music_play;
+                break;
+            case QSConstants.TILE_TRDS:
+                resourceName = "ic_qs_trds_on";
+                stringId = R.string.qs_tile_trds;
                 break;
             default:
                 return null;
