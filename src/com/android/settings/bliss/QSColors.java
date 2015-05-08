fb9753005d1d13d9f6f1954bd23520046d42c792
XXMrHyde <xxmrhyde@gmx.co.uk>
2015-05-08 15:49:26 -0600

    Notification drawer: Custom clear all icon color, (2/2):
    Change-Id: Iaf582ab4b3b4583071bb7c4b0d7db57892d06969

--- src/com/android/settings/bliss/QSColors.java
+++ src/com/android/settings/bliss/QSColors.java
@@ -48,6 +48,8 @@ public class QSColors extends SettingsPreferenceFragment implements
             "qs_transparent_shade";
     private static final String PREF_QS_COLOR_SWITCH =
             "qs_color_switch";
+    private static final String PREF_CLEAR_ALL_ICON_COLOR =
+            "notification_drawer_clear_all_icon_color";
 
     private static final int DEFAULT_BACKGROUND_COLOR = 0xff263238;
     private static final int WHITE = 0xffffffff;
@@ -61,6 +63,7 @@ public class QSColors extends SettingsPreferenceFragment implements
     private ColorPickerPreference mQSTextColor;
     private SwitchPreference mQSShadeTransparency;
     private SwitchPreference mQSSSwitch;
+    private ColorPickerPreference mClearAllIconColor;
 
     private ContentResolver mResolver;
 
@@ -123,6 +126,17 @@ public class QSColors extends SettingsPreferenceFragment implements
                 Settings.System.QS_COLOR_SWITCH, 0) == 1));
         mQSSSwitch.setOnPreferenceChangeListener(this);
 
+        mClearAllIconColor =
+                (ColorPickerPreference) findPreference(PREF_CLEAR_ALL_ICON_COLOR);
+        intColor = Settings.System.getInt(mResolver,
+                Settings.System.NOTIFICATION_DRAWER_CLEAR_ALL_ICON_COLOR, WHITE); 
+        mClearAllIconColor.setNewPreviewColor(intColor);
+        hexColor = String.format("#%08x", (0xffffffff & intColor));
+        mClearAllIconColor.setSummary(hexColor);
+        mClearAllIconColor.setDefaultColors(WHITE, BLISS_BLUE);
+        mClearAllIconColor.setOnPreferenceChangeListener(this);
+        mClearAllIconColor.setAlphaSliderEnabled(true);
+
         setHasOptionsMenu(true);
     }
 
@@ -183,6 +197,14 @@ public class QSColors extends SettingsPreferenceFragment implements
                     Settings.System.QS_COLOR_SWITCH, value ? 1 : 0);
             refreshSettings();
             return true;
+        } else if (preference == mClearAllIconColor) {
+            hex = ColorPickerPreference.convertToARGB(
+                Integer.valueOf(String.valueOf(newValue)));
+            intHex = ColorPickerPreference.convertToColorInt(hex);
+            Settings.System.putInt(mResolver,
+                Settings.System.NOTIFICATION_DRAWER_CLEAR_ALL_ICON_COLOR, intHex);
+            preference.setSummary(hex);
+            return true;
         }
         return false;
     }
@@ -228,6 +250,8 @@ public class QSColors extends SettingsPreferenceFragment implements
                                     Settings.System.QS_TEXT_COLOR, WHITE);
                             Settings.System.putInt(getOwner().mResolver,
                                     Settings.System.QS_TRANSPARENT_SHADE, 0);
+                            Settings.System.putInt(getOwner().mResolver,
+                                    Settings.System.NOTIFICATION_DRAWER_CLEAR_ALL_ICON_COLOR, WHITE);
                             getOwner().refreshSettings();
                         }
                     })
@@ -245,6 +269,9 @@ public class QSColors extends SettingsPreferenceFragment implements
                                     BLISS_BLUE);
                             Settings.System.putInt(getOwner().mResolver,
                                     Settings.System.QS_TRANSPARENT_SHADE, 0);
+                            Settings.System.putInt(getOwner().mResolver,
+                                    Settings.System.NOTIFICATION_DRAWER_CLEAR_ALL_ICON_COLOR,
+                                    BLISS_BLUE);
                             getOwner().refreshSettings();
                         }
                     })
