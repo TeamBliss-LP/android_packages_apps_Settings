a6ba7aedc477fca4db1a0cbded06ed5a3e572b5a
LorDClockaN <davor@losinj.com>
2015-06-25 22:49:25 +0200

    Settings: Torch to powermenu (2/2)
    TeamBliss- Make it a switch
    
    Change-Id: I81d7d90be4c3b9d20cf10b0c1f334aef1f86e135

--- src/com/android/settings/cyanogenmod/PowerMenuActions.java
+++ src/com/android/settings/cyanogenmod/PowerMenuActions.java
@@ -43,6 +43,7 @@ import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.internal.util.cm.PowerMenuConstants;
 import com.android.settings.bliss.NumberPickerPreference;
+import com.android.internal.util.cm.QSUtils;
 
 import static com.android.internal.util.cm.PowerMenuConstants.*;
 
@@ -59,6 +60,7 @@ public class PowerMenuActions extends SettingsPreferenceFragment
     private SwitchPreference mRebootPref;
     private SwitchPreference mScreenshotPref;
     private SwitchPreference mScreenrecordPref;
+    private SwitchPreference mTorchPref;
     private SwitchPreference mProfilePref;
     private SwitchPreference mAirplanePref;
     private SwitchPreference mUsersPref;
@@ -111,6 +113,8 @@ public class PowerMenuActions extends SettingsPreferenceFragment
                 mScreenrecordPref = (SwitchPreference) findPreference(GLOBAL_ACTION_KEY_SCREENRECORD);
             } else if (action.equals(GLOBAL_ACTION_KEY_PROFILE)) {
                 mProfilePref = (SwitchPreference) findPreference(GLOBAL_ACTION_KEY_PROFILE);
+            } else if (action.equals(GLOBAL_ACTION_KEY_TORCH)) {
+                mTorchPref = (SwitchPreference) findPreference(GLOBAL_ACTION_KEY_TORCH);
             } else if (action.equals(GLOBAL_ACTION_KEY_AIRPLANE)) {
                 mAirplanePref = (SwitchPreference) findPreference(GLOBAL_ACTION_KEY_AIRPLANE);
             } else if (action.equals(GLOBAL_ACTION_KEY_USERS)) {
@@ -162,6 +166,14 @@ public class PowerMenuActions extends SettingsPreferenceFragment
 
         if (mAirplanePref != null) {
             mAirplanePref.setChecked(settingsArrayContains(GLOBAL_ACTION_KEY_AIRPLANE));
+        }
+
+        if (mTorchPref != null) {
+            if (!QSUtils.deviceSupportsFlashLight(getActivity())) {
+                getPreferenceScreen().removePreference(findPreference(GLOBAL_ACTION_KEY_TORCH));
+            } else {
+                mTorchPref.setChecked(settingsArrayContains(GLOBAL_ACTION_KEY_TORCH));
+            }
         }
 
         if (mUsersPref != null) {
@@ -224,6 +236,10 @@ public class PowerMenuActions extends SettingsPreferenceFragment
         } else if (preference == mAirplanePref) {
             value = mAirplanePref.isChecked();
             updateUserConfig(value, GLOBAL_ACTION_KEY_AIRPLANE);
+
+        } else if (preference == mTorchPref) {
+            value = mTorchPref.isChecked();
+            updateUserConfig(value, GLOBAL_ACTION_KEY_TORCH);
 
         } else if (preference == mUsersPref) {
             value = mUsersPref.isChecked();
