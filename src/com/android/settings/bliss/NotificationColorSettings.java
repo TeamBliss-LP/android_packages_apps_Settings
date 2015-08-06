457025e155d00dc401c713b4aade86210583c3b9
Shaun <shaunsibu@gmail.com>
2015-08-06 13:01:01 -0600

    Change reset for custom notifications:
    - Make a light setting
    - Make a dark setting.
    - Add a summary to Notifications Color Preference.
    
    Change-Id: Ic46b6bce52e7f6737487a7d0e0fdce20120cd079

--- src/com/android/settings/bliss/NotificationColorSettings.java
+++ src/com/android/settings/bliss/NotificationColorSettings.java
@@ -318,7 +318,7 @@ public class NotificationColorSettings extends SettingsPreferenceFragment implem
                     .setTitle(R.string.reset)
                     .setMessage(R.string.dlg_reset_values_message)
                     .setNegativeButton(R.string.cancel, null)
-                    .setNeutralButton(R.string.dlg_reset_android,
+                    .setNeutralButton(R.string.dlg_reset_light,
                         new DialogInterface.OnClickListener() {
                         public void onClick(DialogInterface dialog, int which) {
                             Settings.System.putInt(getOwner().mResolver,
@@ -346,7 +346,7 @@ public class NotificationColorSettings extends SettingsPreferenceFragment implem
                             getOwner().refreshSettings();
                         }
                     })
-                    .setPositiveButton(R.string.dlg_reset_bliss,
+                    .setPositiveButton(R.string.dlg_reset_dark,
                         new DialogInterface.OnClickListener() {
                         public void onClick(DialogInterface dialog, int which) {
                             Settings.System.putInt(getOwner().mResolver,
@@ -357,7 +357,7 @@ public class NotificationColorSettings extends SettingsPreferenceFragment implem
                                     Settings.System.NOTIFICATION_APP_ICON_COLOR_MODE, 1);
                             Settings.System.putInt(getOwner().mResolver,
                                     Settings.System.NOTIFICATION_BG_COLOR,
-                                    WHITE);
+                                    BLACK);
                             Settings.System.putInt(getOwner().mResolver,
                                     Settings.System.NOTIFICATION_GUTS_BG_COLOR,
                                     SYSTEMUI_SECONDARY);
@@ -366,13 +366,13 @@ public class NotificationColorSettings extends SettingsPreferenceFragment implem
                                     TRANSLUCENT_HOLO_BLUE_LIGHT);
                             Settings.System.putInt(getOwner().mResolver,
                                     Settings.System.NOTIFICATION_TEXT_COLOR,
-                                    HOLO_BLUE_LIGHT);
+                                    WHITE);
                             Settings.System.putInt(getOwner().mResolver,
                                     Settings.System.NOTIFICATION_ICON_COLOR,
-                                    HOLO_BLUE_LIGHT);
+                                    WHITE);
                             Settings.System.putInt(getOwner().mResolver,
                                     Settings.System.NOTIFICATION_DRAWER_CLEAR_ALL_ICON_COLOR,
-                                    HOLO_BLUE_LIGHT);
+                                    WHITE);
                             getOwner().refreshSettings();
                         }
                     })
