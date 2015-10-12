4229af92e492c738d192d854192f60437222c0b6
Shaun Eaton <shaunsibu@gmail.com>
2015-10-12 14:27:41 -0700

    Revert "Fix issue #23116383: Include security patch level in Settings"
    This reverts commit 9d8a3469c4f5ed3daf2fb56647457d6a5ae72011.
    
    Change-Id: Ia4f9cbc4417b558cd96e3741a0156da241ddd4ce

--- src/com/android/settings/DeviceInfoSettings.java
+++ src/com/android/settings/DeviceInfoSettings.java
@@ -89,7 +89,6 @@ public class DeviceInfoSettings extends SettingsPreferenceFragment implements In
     private static final String KEY_BASEBAND_VERSION = "baseband_version";
     private static final String KEY_FIRMWARE_VERSION = "firmware_version";
     private static final String KEY_SECURITY_PATCH = "security_patch";
-    private static final String KEY_UPDATE_SETTING = "additional_system_update_settings";
     private static final String KEY_EQUIPMENT_ID = "fcc_equipment_id";
     private static final String PROPERTY_EQUIPMENT_ID = "ro.ril.fccid";
     private static final String KEY_DEVICE_FEEDBACK = "device_feedback";
